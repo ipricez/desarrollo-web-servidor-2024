d7d0726ab0ab1250cb2ff78ce85b8f70630ff0db
ipricez <fsfcantarero@gmail.com>
2024-09-25 12:36:34 +0200

    Actualizacion listas.php

--- 02_Estructuras_de_control/listas.php
+++ 02_Estructuras_de_control/listas.php
@@ -33,6 +33,51 @@
     CON WHILE Y LAS ESTRUCTURAS DE CONTROL NECESARIAS, MUESTRA EN UNA LISTA SIN ORDENAR
     TODOS LOS MULTIPLOS DE 3 ENTRE 1 Y 3
     */
+    $i = 1;
+    echo "<ul>";
+    while($i <= 30):
+        if($i % 3 == 0):
+            echo "<li>$i</li>";
+        endif;
+        $i++;
+    endwhile;
+    echo "</ul>";
     ?>
+
+    <h3>Lista con DO WHILE</h3>
+    <?php
+    $i = 1;
+
+    echo "<ul>";
+    do {
+        echo "<li>$i</li>";
+        $i++;
+    } while ($i <= 10);
+    echo "</ul>"; 
+    ?>
+
+    <h3>Lista con FOR</h3>
+    <?php
+    echo "<ul>";
+    for ($i = 1; $i <= 10; $i++) {
+        if ($i >= 10) break;
+        echo "<li>$i</li>";
+    }
+    echo "</ul>"
+    ?>
+
+    <h3>Lista con FOR 3</h3>
+    <?php
+    echo "<ul>";
+    $i = 1;
+    for (;;) {
+        if ($i >= 10) break;
+        echo "<li>$i</li>";
+        $i++;
+    }
+    echo "</ul>"
+    ?>
+
+    <h3></h3>
 </body>
 </html>
