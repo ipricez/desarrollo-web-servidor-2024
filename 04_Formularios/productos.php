4ba20cc77f1ec58d713f0c7972278465a0dd0859
ipricez <fsfcantarero@gmail.com>
2024-10-09 14:00:07 +0200

    internet 10-09

---
+++ 04_Formularios/productos.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $productos = [
+            ["Nintendo Switch", 400],
+            ["PS5 Slim", 450],
+            ["PS5 Pro", 800],
+            ["XBOX Series S", 300],
+            ["XBOX Series X", 300],
+        ]
+        /**
+         * Añadir al array una tercera columna que será el stock, y se generará con un rand entre 0 y 5
+         * 
+         * Mostrar en una tabla cada producto junto a su precio y su stock
+         * 
+         * Crear un formulario donde se introduzca el nombre de un producto, y:
+         *  - Si hay stock, decimos que está disponible y su precio.
+         *  - Si no hay, decimos que está agotado.
+         */
+        $p
+    ?>
+</body>
+</html>
